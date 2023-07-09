fce76b0fcd7f7d2bf2ec019b353f4e84af9f7af1
kpds85 <kpds.85@gmail.com>
2023-07-09 08:51:24 +0530

    sample spring boot project with Maven setup

---
+++ demo/src/test/java/com/example/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = App.class)
+@WebAppConfiguration
+public class HelloControllerTest {
+    @Value("${local.server.port}")
+    int port;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void testHello() throws Exception {
+        when().get("/").then()
+                .body(is("Hello World!"));
+    }
+
+    @Test
+    public void testCalc() throws Exception {
+        given().param("left", 100)
+                .param("right", 200)
+                .get("/calc")
+                .then()
+                .body("left", is(100))
+                .body("right", is(200))
+                .body("answer", is(300));
+    }
+}
